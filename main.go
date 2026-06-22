a36a67382bfd36a86d96def2a4bd0f6468bdd0e9
rasyidannas <rasshit.dsgn@gmail.com>
2026-06-22 01:40:38 +0000

    2.3.1 Introducing the Go map & 2.3.2 Writing a table-driven test

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	greeting := greet("en")
+	greeting := greet("id")
 	fmt.Println(greeting)
 }
 
 type language string
 
+var phrasebook = map[language] string {
+	"el": "Χαίρετε Κόσμε",
+	"en": "Hello world",
+	"fr": "Bonjour le monde",
+	"שלום עולם" :"he",
+	"ur": " ہﯿﻠﻮ ",
+	"vi": "Xin chào Thế Giới",
+}
+
 // greet returns a greeting to the world
 func greet(l language) string {
-	switch l {
-		case "en":
-			return "Hello world"
-		case "fr":
-			return "Bonjour le monde"
-		default:
-			return ""
+	greeting, ok := phrasebook[l]
+
+	// fmt.Printf("Ok is %v \n", ok)
+
+	if !ok {
+		return fmt.Sprintf("Unsupported language: %q", l)
 	}
+
+	return greeting
 }
+
