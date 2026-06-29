6e3f0f3545cc706828dd6b457519d77fd15ad18a
rasyidannas <rasshit.dsgn@gmail.com>
2026-06-29 01:22:00 +0000

    4.1.3 The New Function

--- packetlog/level.go
+++ packetlog/level.go
@@ -1,3 +1,5 @@
+package packetlog
+
 type Level byte
 
 const (
--- packetlog/logger.go
+++ packetlog/logger.go
@@ -1,5 +1,23 @@
 package packetlog
 
+// Logger is used to log information
 type Logger struct {
+	threshold Level
+}
 
+// Debugf formats and prints a message if the log level is debug or higher.
+func (l *Logger) Debugf(format string, args ...any){
+	// implement me
+}
+
+// Infof formats and prints a message if the log level is info or higher.
+func (l *Logger) Infof(format string, args ...any) {
+	// implement me
+}
+
+// New returns you a logger, ready to log at the required threshold.
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold
+	}
 }
