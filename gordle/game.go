628b73b3896353042ec80a597eeea20795216ac7
rasyidannas <rasshit.dsgn@gmail.com>
2026-09-13 23:53:23 +0000

    5.1.3 Isolate the Check

--- gordle/game.go
+++ gordle/game.go
@@ -33,6 +33,17 @@ func (g *Game) Play() {
 
 const solutionLength = 5
 
+// errInvalidWordLength is returned when the guess has the wrong number of chracters
+var errInvalidWordLength = fmt.Errorf("invalid guess, word doesn't have the correct number of letters")
+
+// validateGuess ensures the guess is valid enough.
+func (g *Game) validateGuess(guess []rune) error {
+	if len(guess) != solutionLength {
+		return fmt.Errorf("expected %d, got %d, %w", solutionLength, len(guess), errInvalidWordLength)
+	}
+	return nil
+}
+  
 // ask reads input until a valid suggestion is made (and returned).
 func (g *Game) ask() []rune {
 	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
@@ -46,7 +57,8 @@ func (g *Game) ask() []rune {
 
 		guess := []rune(string(playerInput))
 
-		if len(guess) != solutionLength {
+		err = g.validateGuess(guess)
+		if err != nil {
 			_, _ = fmt.Fprintf(os.Stderr, "Your attempt is invalid with Gordle's solution: ")
 		} else {
 			return guess
