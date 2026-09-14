559febc33c45260fe99b1cd8ff4efe61b2d2e645
rasyidannas <rasshit.dsgn@gmail.com>
2026-09-14 00:20:45 +0000

    5.16 Test validateGuess method

--- gordle/game_internal_test.go
+++ gordle/game_internal_test.go
@@ -1,11 +1,39 @@
 package gordle
 
 import (
+	"errors"
 	"strings"
 	"testing"
 
 	"golang.org/x/exp/slices"
 )
+
+func TestGameValidateGuess(t *testing.T) {
+	tt := map[string]struct {
+		word []rune
+		expected error
+	}{
+		"nominal": {
+			word: []rune("GUESS"),
+			expected: nil,
+		},
+		"too long": {
+			word: []rune("POCKET"),
+			expected: errInvalidWordLength,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := New(nil)
+
+			err := g.validateGuess(tc.word)
+			if !errors.Is(err, tc.expected) {
+				t.Errorf("%c, expected %q, got %q", tc.word, tc.expected, err)
+			}	
+		})
+	}
+}
 
 func TestGameAsk(t *testing.T) {
 	tt := map[string]struct {
