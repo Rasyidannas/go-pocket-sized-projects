06a508bd8961ea03ce61b74147b685519e6598b9
rasyidannas <rasshit.dsgn@gmail.com>
2026-09-23 02:04:36 +0000

    5.3.2 Read the corpus

---
+++ gordle/corpus_test.go
@@ -0,0 +1,37 @@
+package gordle
+
+import (
+	"testing"
+)
+
+func TestReadCorpus(t *testing.T) {
+	tt := map[string]struct {
+		file string
+		length int
+		err error
+	}{
+		"English corpus": {
+			file: "../corpus/english.txt",
+			length: 35,
+			err: nil,
+		},
+		"empty corpus": {
+			file: "../corpus/empty.txt",
+			length: 0,
+			err: ErrCorpusIsEmpty,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			words, err := ReadCorpus(tc.file)
+			if tc.err != err {
+				t.Errorf("expected err %v, got %v", tc.err, err)
+			}
+
+			if tc.length != len(words) {
+				t.Errorf("expected %d, got %d", tc.length, len(words))
+			}
+		})
+	}
+}
